6835ee5dfa76eeac6a38a471695ef8c821a4e824
SmolyanovM <ratlord85@gmail.com>
2021-03-27 14:50:50 +0600

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let name = [];
+  members.forEach(function (member) {
+    if (typeof member == "string") name.push(member.trim()[0].toUpperCase());
+  });
+  return name.sort().join("");
 };
